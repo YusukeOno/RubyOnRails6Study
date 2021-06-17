93024b0ef46b970788d79faf6a9db3ddd3830d30
小野裕輔 <y-ono@nswt.co.jp>
2021-06-17 22:10:46 +0900

    fixed typo

--- apps/baukis2/config/initializers/baukis2.rb
+++ apps/baukis2/config/initializers/baukis2.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
     config.baukis2 = {
         staff: { host: "baukis2.example.com", path: "" },
-        admin: { host: "baukis2.example.com", patj: "admin" },
+        admin: { host: "baukis2.example.com", path: "admin" },
         customer: { host: "example.com", path: "mypage" }
     }    
 end
