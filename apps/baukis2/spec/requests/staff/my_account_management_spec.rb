a388c0bdd7ddedebd7fa20f2846056ca3b820483
小野裕輔 <y-ono@nswt.co.jp>
2021-06-24 22:00:24 +0900

    Chapter 12.3.3まで

--- apps/baukis2/spec/requests/staff/my_account_management_spec.rb
+++ apps/baukis2/spec/requests/staff/my_account_management_spec.rb
@@ -14,6 +14,20 @@ describe "職員による自分のアカウントの管理" do
                 }
             }
         end
+    describe "情報表示" do
+        let(:staff_member) { create(:staff_member) }
+
+        example "成功" do
+            get staff_account_url
+            expect(response.status).to eq(200)
+        end
+
+        example "停止フラグがセットされたら強制的にログアウト" do
+            staff_member.update_column(:suspended, true)
+            get staff_account_url
+            expect(response).to redirect_to(staff_root_url)
+        end
+    end
 
     describe "更新" do
         let(:params_hash) { attributes_for(:staff_member) }
@@ -28,7 +42,7 @@ describe "職員による自分のアカウントの管理" do
         end
 
         example "例外ActionController::ParameterMissingが発生" do
-      expect { patch staff_account_url, params: { id: staff_member.id } }.
+            expect { patch staff_account_url, params: { id: staff_member.id } }.
             to raise_error(ActionController::ParameterMissing)
         end
 
