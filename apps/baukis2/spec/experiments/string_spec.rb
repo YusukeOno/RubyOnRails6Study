9d8f8c8234d20aec778bc2c5574cb8afa6944232
小野裕輔 <y-ono@nswt.co.jp>
2021-07-05 08:46:53 +0900

    Chapter 13.5まで

--- apps/baukis2/spec/experiments/string_spec.rb
+++ apps/baukis2/spec/experiments/string_spec.rb
@@ -1,16 +1,16 @@
 require "spec_helper"
 
 describe String do
-    describe "#<<" do
-        it "文字の追加" do
-            s = "ABC"
-            s << "D"
-            expect(s.size).to eq(4)
-        end
-
-        example "nilの追加はできない", :exception do
-            s = "ABC"
-            expect { s << nil }.to raise_error(TypeError)
-        end
+  describe "#<<" do
+    example "文字の追加" do
+      s = "ABC"
+      s << "D"
+      expect(s.size).to eq(4)
     end
+
+    example "nilは追加できない", :exception do
+      s = "ABC"
+      expect { s << nil }.to raise_error(TypeError)
+    end
+  end
 end
